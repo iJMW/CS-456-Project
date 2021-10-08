9a1ff1d5fbc604a79f3de94fdad885570a276471
Jalen Wayt <jalen.wayt@gmail.com>
2021-10-07 21:52:54 -0400

    Base File for part 1

---
+++ app.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def main():
+    print("placeholder")
+
+def noise_reduction():
+    print("placeholder")
+
+def non_max_suppression():
+    print("Placeholder")
+
+def gradient_calculation():
+    print("placeholder")
+
+def double_threshold():
+    print("placeholder")
+
+def hysteresis():
+    print("placeholder")
+
+if __name__ == "__main__":
+    main()
