de21f53fd4743c85bd75aee1aac91d2761d36312
Jalen Wayt <jalen.wayt@gmail.com>
2021-10-12 19:43:49 -0400

    Implemented Non Max Suppression Draft

--- app.py
+++ app.py
@@ -7,8 +7,50 @@ def main():
 def noise_reduction():
     print("placeholder")
 
-def non_max_suppression():
-    print("Placeholder")
+#Check angle parameter?
+def non_max_suppression(img, angleDirection):
+    #Gets the size of the image
+    (row, col) = img.Shape
+    #Creates a matrix of zeros
+    nonMaxMatrix = np.zeros(row, col)
+
+    #Iterates over all the pixels
+    for i in range(1, row-1):
+        for j in range(1, col-1):
+            #Assigns the neighboring pixels adjacent to the current pixel
+            prevPixel = img[i][j]
+            nextPixel = img[i][j]
+            #Assigns the angle of the pixels from the gradient step
+            angle = angleDirection[i][j]
+
+            #Checks the pixels on the left and right
+            if((0 <= angleDirection < np.pi/4) or (7*np.pi/4 <= angleDirection < 2*pi)):
+                prevPixel = img[i, j-1]
+                nextPixel = img[i, j+1]
+            #Checks the pixels on the top right and bottom left
+            elif((np.pi/4 <= angleDirection < np.pi/2) or (5*np.pi/4 <= angleDirection < 3*np.pi/2)):
+                prevPixel = img[i+1, j-1]
+                nextPixel = img[i-1, j+1]
+            #Checks the pixels on the top middle and bottom middle
+            elif((np.pi/2 <= angleDirection < 3*np.pi/4) or (3*np.pi/2 <= angleDirection < 7*np.pi/2)):
+                prevPixel = img[i-1, j-1]
+                nextPixel = img[i+1, j+1]
+            #Checks the pixels on the top left and bottom right
+            else:
+                prevPixel = img[i-1, j-1]
+                nextPixel = img[i+1, j+1]
+            
+            if((img[i, j] >= prevPixel) and (img(i,j) >= nextPixel)):
+                nonMaxMatrix[i, j] = img[i][j]   
+            
+
+
+    return nonMaxMatrix
+
+
+
+
+
 
 def gradient_calculation():
     print("placeholder")
