a328a95d91e39a4a34c28e6ad57b83d190027a45
48fan48 <Fan4899@gmail.com>
2021-10-12 20:10:58 -0400

    Addeadded Sobel filtering gradient calculations

--- app.py
+++ app.py
@@ -24,7 +24,17 @@ def noise_reduction(size, sigma):
     
 
 def non_max_suppression(img):
-    print("placeholder")
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+
+    Ix = ndimage.filters.convolve(img, Kx)
+    Iy = ndimage.filters.convolve(img, Ky)
+
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+    
+    return (G, theta)
 
 def gradient_calculation():
     print("placeholder")
